462ba454607c95860326f911b55e3f988b656c92
Nnamdi Onye <84255559+NnamOnye@users.noreply.github.com>
2024-05-27 12:54:25 -0500

    Add files via upload

---
+++ Throttle Position Sensor C++.cpp
@@ -0,0 +1,37 @@
+/*PASTE AND RUN CODE HERE*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+//In a realworld applocation this function would be changed
+//It will interact with the hardware
+int readThrottlePositionSensor() {
+
+    return rand() % 1024;
+}
+
+// Converting ADC value to throttle position percentage
+double convertToThrottlePosition(int adcValue) {
+    const int ADC_MAX = 1023;  
+    return (static_cast<double>(adcValue) / ADC_MAX) * 100.0;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(0)));  
+
+    while (true) {
+        int adcValue = readThrottlePositionSensor();  
+        double throttlePosition = convertToThrottlePosition(adcValue);  
+
+        cout << "ADC Value: " << adcValue << " -> Throttle Position: " << throttlePosition << "%" << endl;
+
+
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+
+    return 0;
+}
